114b610f98155f5a36e21e77008374e57500122b
aryanjain77 <aj03741801@gmail.com>
2024-07-31 09:55:55 +0530

    files added

---
+++ index.js
@@ -0,0 +1,69 @@
+const path = require("path");
+const getYouTubeID = require('get-youtube-id');
+const { getSubtitles } = require('youtube-captions-scraper');
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const express = require("express");
+const rateLimit = require("express-rate-limit");
+const API_KEY = process.env.gemAPIs;
+var dres;
+// https://youtu.be/N_sUsq_y10U?si=JXdwUBKxef7K3DiC
+
+
+const genAi = new GoogleGenerativeAI(API_KEY);
+const model = genAi.getGenerativeModel({ model: "gemini-1.5-flash"});
+
+const apiLimiter = rateLimit({
+  windowMS : 24 * 60 * 60 * 1000,
+  max: 100,
+  message: "Too many requests try again later",
+});
+
+async function fetchSubtitles(u,res) {
+  try {
+    let urlQuery = u;
+    let id = await getYouTubeID(urlQuery);
+    let captionsData;
+    let caption = await getSubtitles({
+      videoID: id,
+      lang: 'en'
+    });
+    caption.forEach(caption => {
+      captionsData += (`${caption.text}`);
+    });
+    const prompt = "you are a text to studybook converter. You will be taking the text and provide highly detailed explanations in points within 300 words in the form of study notes. If same text is given again generate again with some changes. The text will be appended here  ";
+    const result = await model.generateContent(captionsData,prompt);
+    dres = result.response.text();
+    // res.json(result.response.text());
+    // let abstract = sum({ 'corpus': captionsData });
+    // res.send(abstract.summary);
+  } catch (error) {
+    console.error('Error in fetching subtitles:', error);
+  }
+}
+
+// async function toHtml(text) {
+//   text.replaceAll("\\n","<br>");
+// }
+
+const app = express();
+app.use('/', apiLimiter);
+app.use(express.static('./public'));
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req,res) => {
+  res.sendFile(path.join(process.cwd(),"./public/index.html"));
+});
+
+app.get("/api", apiLimiter, (req,res) => {
+  let { url } = req.query;
+  fetchSubtitles(url,res);
+  res.json(dres);
+});
+
+// app.get('/display', (req,res) => {
+//   let { url } = req.query;
+//   let data = fetchSubtitles(url,res);
+//   res.render('display', { data: JSON.stringify(data) });
+// });
+
+app.listen(8000,"127.0.0.1",() => console.log(`http://127.0.0.1:8000`));
---
+++ public/index.js
@@ -0,0 +1,153 @@
+// List of sentences
+var _CONTENT = [
+	"Effective...",
+	"Amazing...",
+	"Optimized..."
+];
+
+// Current sentence being processed
+var _PART = 0;
+
+// Character number of the current sentence being processed
+var _PART_INDEX = 0;
+
+// Holds the handle returned from setInterval
+var _INTERVAL_VAL;
+
+// Element that holds the text
+var _ELEMENT = document.querySelector("#text");
+
+// Cursor element
+var _CURSOR = document.querySelector("#cursor");
+
+// Implements typing effect
+function Type() {
+	// Get substring with 1 characater added
+	var text =  _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+	_ELEMENT.innerHTML = text;
+	_PART_INDEX++;
+
+	// If full sentence has been displayed then start to delete the sentence after some time
+	if(text === _CONTENT[_PART]) {
+		// Hide the cursor
+		_CURSOR.style.display = 'none';
+
+		clearInterval(_INTERVAL_VAL);
+		setTimeout(function() {
+			_INTERVAL_VAL = setInterval(Delete, 50);
+		}, 1000);
+	}
+}
+
+// Implements deleting effect
+function Delete() {
+	// Get substring with 1 characater deleted
+	var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+	_ELEMENT.innerHTML = text;
+	_PART_INDEX--;
+
+	// If sentence has been deleted then start to display the next sentence
+	if(text === '') {
+		clearInterval(_INTERVAL_VAL);
+
+		// If current sentence was last then display the first one, else move to the next
+		if(_PART == (_CONTENT.length - 1))
+			_PART = 0;
+		else
+			_PART++;
+
+		_PART_INDEX = 0;
+
+		// Start to display the next sentence after some time
+		setTimeout(function() {
+			_CURSOR.style.display = 'inline-block';
+			_INTERVAL_VAL = setInterval(Type, 100);
+		}, 200);
+	}
+}
+
+// Start the typing effect on load
+_INTERVAL_VAL = setInterval(Type, 100);
+
+function showLoader() {
+  var btn = document.querySelector("#form-submit");
+	btn.classList.add("loader");
+	btn.innerText = "";
+}
+
+function hideLoader() {
+	var btn = document.querySelector("#form-submit");
+	var form = document.getElementById('form');
+	btn.classList.remove("loader");
+	btn.classList.add("hidden");
+	form.classList.add("hidden");
+	var lastbuttons = document.getElementById("two-buttons");
+	lastbuttons.classList.remove("hidden");
+}
+
+function hideLoaderInError() {
+	var btn = document.querySelector("#form-submit");
+	var form = document.getElementById('form');
+	btn.classList.remove("loader");
+	btn.innerText = "Submit";
+	var errMsg = document.createElement("p");
+	errMsg.classList.add("errData");
+	errMsg.innerHTML = "<ul><li>Error fetching data</li><li>try using a different url or refreshing the site</li><li>if none of the above methods work it maybe a server error</li></ul>";
+	form.appendChild(errMsg);
+	setTimeout(location.refresh,5000);
+}
+
+
+var query;
+
+document.addEventListener('DOMContentLoaded', (query) => {
+		const form = document.getElementById('form');
+		var input = document.getElementById('form-url');
+		var container = document.getElementById('data');
+		form.addEventListener('submit', async (e) => {
+				e.preventDefault();
+				query = input.value.trim();
+				if (query) {
+						try {
+								showLoader();
+								const response = await fetch(`/api/?url=${encodeURIComponent(query)}`);
+								const data = await response.json();
+								const stringdata = JSON.stringify(data);
+								const formatted = stringdata.replaceAll("\\n","<br>");
+								const final = formatted.replaceAll(/\*(.*?)\*/g, '<b>$1</b>');
+								container.innerHTML = `<b style="display:flex;align-items:center;justify-content:center;font-size:40px;margin-bottom:50px;line-height:20px;">Study Notes</b><br> ${final}`;
+								hideLoader();
+						} catch (error) {
+								console.error('Error fetching data:', error);
+								hideLoaderInError();
+						}
+				}
+		});
+});
+
+document.addEventListener('DOMContentLoaded', (query) => {
+		const form = document.getElementById('form');
+		var input = document.getElementById('form-url');
+		var resubmit = document.getElementById('btn1');
+		var container = document.getElementById('data');
+		resubmit.addEventListener('click', async (e) => {
+				e.preventDefault();
+				query = input.value.trim();
+				if (query) {
+						try {
+								showLoader();
+								const response = await fetch(`/api/?url=${encodeURIComponent(query)}`);
+								const data = await response.json();
+								const stringdata = JSON.stringify(data);
+								const formatted = stringdata.replaceAll("\\n","<br>");
+								final = formatted.replaceAll(/\*(.*?)\*/g, '<b>$1</b>');
+								// container.innerHTML = JSON.stringify(data);
+								container.innerHTML = `<b style="display:flex;align-items:center;justify-content:center;font-size:40px;margin-bottom:50px;line-height:20px;">Study Notes</b><br> ${final}`;
+								hideLoader();
+						} catch (error) {
+								console.error('Error fetching data:', error);
+								hideLoaderInError();
+						}
+				}
+		});
+});
